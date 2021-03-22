363a25cef271ce96c5c56d1a35c7da46c52760ef
ernesto <ernesto@Vava-Precision-7710>
2021-03-22 15:47:54 +0000

    Material for lab sesion 9

--- lab9/load_embeddings.py
+++ lab9/load_embeddings.py
@@ -4,5 +4,25 @@ from gensim.models import KeyedVectors
 wv = KeyedVectors.load("pizza.embeddings", mmap='r')
 
 
-vector = wv['pizza22']  # Get numpy vector of a word
+vector = wv['pizza']  # Get numpy vector of a word
 print(vector)
+
+
+#cosine similarity
+similarity = wv.similarity('pizza', 'http://www.co-ode.org/ontologies/pizza/pizza.owl#Pizza')
+print(similarity)
+
+similarity = wv.similarity('http://www.co-ode.org/ontologies/pizza/pizza.owl#Margherita', 'margherita')
+print(similarity)
+
+
+
+
+
+#Most similar cosine similarity
+result = wv.most_similar(positive=['margherita', 'pizza'])
+print(result)
+
+#Most similar entities: cosmul
+result = wv.most_similar_cosmul(positive=['margherita'])
+print(result)
